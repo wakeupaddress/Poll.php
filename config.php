5e1a6d5f20e6719716866fa8d09e8053ebe9de8a
wakeup <wakeupaddress@gmail.com>
2020-02-11 17:34:23 +0900

    first commit

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+ini_set('display_errors', 1);
+
+define('DSN', 'mysql:host=localhost;dbname=dotinstall_poll_php');
+define('DB_USERNAME', 'dbuser');
+define('DB_PASSWORD', 't8CmBz4A');
+
+session_start();
+
+require_once(__DIR__ . '/functions.php');
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>Poll</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <h1>Which do you like best?</h1>
+  <form action="" method="post">
+    <div class="row">
+      <div class="box" id="box_0" data-id="0"></div>
+      <div class="box" id="box_1" data-id="1"></div>
+      <div class="box" id="box_2" data-id="2"></div>
+    </div>
+    <div id="btn">Vote and See Results</div>
+  </form>
+</body>
+</html>
